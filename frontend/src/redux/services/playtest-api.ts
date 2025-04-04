0ee66af279110e781136ea721614a1e0fa1b8973
tlx02 <e0774379@u.nus.edu>
2025-04-04 11:22:02 +0800

    Revert "Error Fixes"
    This reverts commit 9f8aaaeeadc09206c648df74662189242bcfc3dc.

--- frontend/src/redux/services/playtest-api.ts
+++ frontend/src/redux/services/playtest-api.ts
@@ -1,15 +1,15 @@
 import {
-  PlaytestPostData,
+  FeedbackData,
   FeedbackInitialResponseData,
   FeedbackInitialResponsePostData,
-  PlaytestResponseData,
+  FeedbackPostData,
 } from "../../types/feedback";
 import baseApi from "./base-api";
 
 const playtestApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     // Note: We use the full URL here for the playtest query endpoint.
-    getFeedback: build.query<PlaytestResponseData, PlaytestPostData>({
+    getFeedback: build.query<FeedbackData, FeedbackPostData>({
       query: (data) => ({
         url: "https://lightrag-718956186327.asia-southeast1.run.app/query",
         method: "POST",
@@ -18,7 +18,7 @@ const playtestApi = baseApi.injectEndpoints({
     }),
     createInitialResponseIfNotExists: build.mutation<FeedbackInitialResponseData, FeedbackInitialResponsePostData>({
       query: (feedbackPostData) => ({
-        url: "/feedback/initial-response/", // if this endpoint should use your baseUrl, keep it relative
+        url: "/playtest/initial-response/", // if this endpoint should use your baseUrl, keep it relative
         method: "POST",
         body: feedbackPostData,
       }),
